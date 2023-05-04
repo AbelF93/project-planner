9b2641b6020e6d6c29a9e886a5e7e96731eab9a7
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-04 15:46:40 +0200

    Add function sortTasks

--- assets/js/modules/task_creation.js
+++ assets/js/modules/task_creation.js
@@ -2,9 +2,10 @@ const inputList = document.querySelector("input");
 const taskSection = document.createElement("section");
 
 taskSection.innerHTML = `<ul class="task-list"></ul>`;
-const taskList = taskSection.querySelector(".task-list");
+export const taskList = taskSection.querySelector(".task-list");
 const body = document.querySelector("body");
 body.appendChild(taskSection);
+
 export const taskButton = document.getElementsByClassName("task-button")[0];
 
 export function addList() {
@@ -14,6 +15,7 @@ export function addList() {
     const taskItem = document.createElement("li");
     taskItem.classList.add("task-list__task");
     taskItem.innerHTML = inputList.value;
+    taskItem.dataset.duedate = "2023-10-20"; // Ajout après push
     taskList.appendChild(taskItem);
   }
   inputList.value = "";
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,43 @@
 import { daysStay } from "./modules/daysStay.js";
 import { addList, taskButton } from "./modules/task_creation.js";
+import { sortTasks } from "./sortTasks.js";
 
-// Add Button Task
-taskButton.addEventListener('click', addList);
+
+// Button add Task
+taskButton.addEventListener("click", addList);
 
 // Add Due Date
 const dueDate = "2023-10-20";
 const days = daysStay(dueDate);
 const message = document.createElement("p");
+// Pour tester le nb de jours restants en brut
 message.textContent = `Il reste ${days} jours avant la date limite.`;
 const body = document.querySelector("body");
 body.appendChild(message);
 
-//
+const sortContainer = document.createElement("div");
+
+const sortLabel = document.createElement("label");
+sortLabel.textContent = "Trier par :";
+sortContainer.appendChild(sortLabel);
+
+const sortSelect = document.createElement("select");
+sortSelect.addEventListener("change", sortTasks);
+sortContainer.appendChild(sortSelect);
+
+const sortOptionName = document.createElement("option");
+sortOptionName.value = "name";
+sortOptionName.textContent = "Nom";
+sortSelect.appendChild(sortOptionName);
+
+const sortOptionDays = document.createElement("option");
+sortOptionDays.value = "days";
+sortOptionDays.textContent = "Jours restants";
+sortSelect.appendChild(sortOptionDays);
+
+const sortButton = document.createElement("button");
+sortButton.textContent = "Trier";
+sortButton.addEventListener("click", sortTasks);
+sortContainer.appendChild(sortButton);
+
+body.appendChild(sortContainer);
