54ec73b61f6dc8c52092cfef9c761f9cae420f50
Abel Fortunati <abel.fortunati@gmail.com>
2023-05-04 17:37:44 +0200

    three input list generation

--- assets/js/modules/task_creation.js
+++ assets/js/modules/task_creation.js
@@ -1,4 +1,7 @@
-const inputList = document.querySelector("input");
+const inputList = document.getElementById("task-storage");
+const inputContent = document.getElementById("task-content");
+const inputDate = document.getElementById("task-date");
+
 const taskSection = document.createElement("section");
 
 taskSection.innerHTML = `<ul class="task-list"></ul>`;
@@ -8,13 +11,23 @@ body.appendChild(taskSection);
 export const taskButton = document.getElementsByClassName("task-button")[0];
 
 export function addList() {
-  if (inputList.value === "") {
+  if ((inputList.value === "")||(inputContent.value === "")||(inputDate.value === "")) {
     alert("Please, write something.");
   } else {
     const taskItem = document.createElement("li");
     taskItem.classList.add("task-list__task");
     taskItem.innerHTML = inputList.value;
+    const taskContent = document.createElement("p");
+    taskContent.classList.add("task-list__content");
+    taskContent.innerHTML = inputContent.value;
+    const taskDate = document.createElement("p");
+    taskDate.classList.add("task-list__date");
+    taskDate.innerHTML = inputDate.value;
+    taskItem.appendChild(taskContent);
+    taskItem.appendChild(taskDate);
     taskList.appendChild(taskItem);
   }
   inputList.value = "";
+  inputContent.value = "";
+  inputDate.value = "";
 }
