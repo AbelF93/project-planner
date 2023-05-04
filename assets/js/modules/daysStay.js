91a05805a0b876e498c2fbc376925e9567fc9715
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-04 13:33:05 +0200

    Add function daysStay

---
+++ assets/js/modules/daysStay.js
@@ -0,0 +1,7 @@
+export function daysStay(dueDate) {
+    const dueDateObj = new Date(dueDate);
+    const now = new Date();
+    const timeStay = dueDateObj.getTime() - now.getTime();
+    const daysStay = Math.floor(timeStay / (1000 * 60 * 60 * 24));
+    return daysStay;
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
+import { daysStay } from "./modules/daysStay.js";
 import { addList, taskButton } from "./modules/task_creation.js";
 
+// Add Button Task
 taskButton.addEventListener('click', addList);
 
+// Add Due Date
+const dueDate = "2023-10-20";
+const days = daysStay(dueDate);
+const message = document.createElement("p");
+message.textContent = `Il reste ${days} jours avant la date limite.`;
+const body = document.querySelector("body");
+body.appendChild(message);
 
-
+//
