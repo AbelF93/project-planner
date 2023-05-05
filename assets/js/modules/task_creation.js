6e5f93a5888d7e2629a3fa4952704cff37c15964
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-05 20:35:47 +0200

    Modify button sort, button delete et dates

--- assets/js/modules/task_creation.js
+++ assets/js/modules/task_creation.js
@@ -1,12 +1,9 @@
-
-//import { daysStay } from "./daysStay.js";
+import { daysStay } from "./daysStay.js";
 import { deleteTaskHandler } from "./deleteTaskHandler.js";
-
 
 const inputList = document.getElementById("task-storage");
 const inputContent = document.getElementById("task-content");
 const inputDate = document.getElementById("task-date");
-
 
 const taskSection = document.createElement("section");
 
@@ -16,51 +13,38 @@ const body = document.querySelector("body");
 body.appendChild(taskSection);
 
 export const taskButton = document.getElementsByClassName("task-button")[0];
-export const deleteBtn = document.getElementsByClassName('delete-task');
+export const deleteBtn = document.getElementsByClassName("delete-task");
 
 export function addList() {
-  if ((inputList.value === "")||(inputContent.value === "")||(inputDate.value === "")) {
+  if (
+    inputList.value === "" ||
+    inputContent.value === "" ||
+    inputDate.value === ""
+  ) {
     alert("Please, write something.");
   } else {
     const taskItem = document.createElement("li");
     taskItem.classList.add("task-list__task");
     taskItem.innerHTML = inputList.value;
-/*
-    const deleteTask = document.createElement("button");
-    deleteTask.type = 'button';
-    deleteTask.classList.add('delete-task');
-    deleteBtn.addEventListener(deleteTaskHandler);*/
 
     const taskContent = document.createElement("p");
     taskContent.classList.add("task-list__content");
     taskContent.innerHTML = inputContent.value;
     const taskDate = document.createElement("p");
-   taskDate.classList.add("task-list__date");
-   taskDate.innerHTML = inputDate.value;
+    taskDate.classList.add("task-list__date");
+    taskDate.innerHTML = inputDate.value;
     taskItem.appendChild(taskContent);
     taskItem.appendChild(taskDate);
-   /* taskItem.appendChild(deleteTask);*/
     taskList.appendChild(taskItem);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-task");
+    deleteBtn.innerHTML = "Delete";
+    deleteBtn.addEventListener("click", deleteTaskHandler);
+    taskItem.appendChild(deleteBtn);
   }
 
   inputList.value = "";
   inputContent.value = "";
   inputDate.value = "";
-
 }
-
-/*
-const inputElement = document.createElement("input");
-inputElement.type = "date";
-inputElement.id = "dueDate";
-const message = document.createElement("p");
-
-
-inputElement.addEventListener("change", () => {
-  const dueDate = document.querySelector("#dueDate").value;
-  const days = daysStay(dueDate);
-  message.textContent = `Il reste ${days} jours avant la date limite.`;
-});
-
-//body.appendChild(inputElement);
-//body.appendChild(message);*/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,32 @@
-//import { daysStay } from "./modules/daysStay.js";
+import { daysStay } from "./modules/daysStay.js";
 import { addList, taskButton } from "./modules/task_creation.js";
-//import { sortTasks } from "./sortTasks.js";
+import { sortTasks } from "./sortTasks.js";
 
 
 // Button add Task
 taskButton.addEventListener("click", addList);
 
-
-/*
 const message = document.createElement("p");
+message.classList.add('task-list__date__echeance')
 
 const body = document.querySelector("body");
 body.appendChild(message);
+
+const dueDateElem = document.querySelector(".task-list__date");
+if (dueDateElem !== null) {
+  const dueDateStr = dueDateElem.textContent.trim();
+  const dueDateObj = new Date(dueDateStr);
+  const days = daysStay(dueDateObj);
+  message.textContent = `Days remaining: ${days}`;
+  dueDateElem.parentNode.appendChild(message);
+}
+
 
 // Add sortTask
 const sortContainer = document.createElement("div");
 
 const sortLabel = document.createElement("label");
-sortLabel.textContent = "Trier par :";
+sortLabel.textContent = "Sort by :";
 sortContainer.appendChild(sortLabel);
 
 const sortSelect = document.createElement("select");
@@ -26,39 +35,18 @@ sortContainer.appendChild(sortSelect);
 
 const sortOptionName = document.createElement("option");
 sortOptionName.value = "name";
-sortOptionName.textContent = "Nom";
+sortOptionName.textContent = "Name";
 sortSelect.appendChild(sortOptionName);
 
 const sortOptionDays = document.createElement("option");
 sortOptionDays.value = "days";
-sortOptionDays.textContent = "Jours restants";
+sortOptionDays.textContent = "Days remaining";
 sortSelect.appendChild(sortOptionDays);
 
 const sortButton = document.createElement("button");
-sortButton.textContent = "Trier";
+sortButton.textContent = "Sort";
 sortButton.addEventListener("click", sortTasks);
 sortContainer.appendChild(sortButton);
 
-body.appendChild(sortContainer);
+document.body.appendChild(sortContainer);
 
-// Add localStorage
-const taskInput = document.getElementById("task-storage");
-const taskList = document.querySelector(".task-list");
-
-if (localStorage.getItem("tasks")) {
-  taskList.innerHTML = localStorage.getItem("tasks");
-}
-
-taskInput.addEventListener("keyup", function(event) {
-  if (event.key === "Enter") {
-    const taskName = taskInput.value;
-
-    // Create a new task element
-    const taskItem = document.createElement("li");
-    taskItem.classList.add("task-list__task");
-    taskItem.innerHTML = taskName;
-    taskList.appendChild(taskItem);
-    localStorage.setItem("tasks", taskList.innerHTML);
-    taskInput.value = "";
-  }
-});*/
