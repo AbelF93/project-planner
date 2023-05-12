d09608c4f518ee63ee030ceaed1aee96b1754361
Abel Fortunati <abel.fortunati@gmail.com>
2023-05-12 11:14:40 +0200

    Status Checkboxes integration

--- assets/js/modules/task_creation.js
+++ assets/js/modules/task_creation.js
@@ -1,21 +1,49 @@
-/*
-const inputList = document.querySelector("input");
+
+import { daysStay } from "./daysStay.js";
+import { deleteTaskHandler } from "./deleteTaskHandler.js";
+
+const inputList = document.getElementById("task-storage");
+const inputContent = document.getElementById("task-content");
+const inputDate = document.getElementById("task-date");
+
 const taskSection = document.createElement("section");
 
 taskSection.innerHTML = `<ul class="task-list"></ul>`;
-const taskList = taskSection.querySelector(".task-list");
+export const taskList = taskSection.querySelector(".task-list");
 const body = document.querySelector("body");
 body.appendChild(taskSection);
+
 export const taskButton = document.getElementsByClassName("task-button")[0];
+export const deleteBtn = document.getElementsByClassName("delete-task");
+const inputDueDate = document.getElementById("task-due-date");
 
 export function addList() {
-  if (inputList.value === "") {
+  if (
+    inputList.value === "" ||
+    inputContent.value === "" ||
+    inputDate.value === ""
+  ) {
     alert("Please, write something.");
-  } else {
+  } else {//Create a task name
     const taskItem = document.createElement("li");
     taskItem.classList.add("task-list__task");
-    taskItem.textContent = inputList.value;
-        const statusCheckboxes = document.createElement('div');
+    taskItem.innerHTML = inputList.value;
+    taskItem.dataset.duedate = inputDate.value;
+    // Create a description
+    const taskContent = document.createElement("p");
+    taskContent.classList.add("task-list__content");
+    taskContent.innerHTML = inputContent.value;
+    // Create a date
+    const taskDate = document.createElement("p");
+    taskDate.classList.add("task-list__date");
+    taskDate.innerHTML = inputDate.value;
+    // Add the remaining days 
+    const remainingDays = daysStay(inputDate.value);
+    const taskDays = document.createElement("p");
+    taskDays.classList.add("task-list__date__due");
+    taskDays.innerHTML = `Days remaining: ${remainingDays}`;
+    //Add Checkboxes
+    const statusCheckboxes = document.createElement('div');
         statusCheckboxes.setAttribute("class","task__checkboxes");
               const toDoStatusBox = document.createElement('div');
               toDoStatusBox.setAttribute("class","status-checkbox");
@@ -47,7 +75,8 @@ export function addList() {
                     doneLabel.setAttribute("for","done");
                     doneLabel.setAttribute("id","label__done");
                     const doneLabelText = document.createTextNode('Done');
-          doneLabel.appendChild(doneLabelText);
+       // Parenting
+       doneLabel.appendChild(doneLabelText);
        doneStatusBox.appendChild(doneLabel);
        doneStatusBox.appendChild(doneCheckbox);
      statusCheckboxes.appendChild(doneStatusBox);
@@ -60,54 +89,6 @@ export function addList() {
        toDoStatusBox.appendChild(toDoCheckbox);
      statusCheckboxes.appendChild(toDoStatusBox);
      taskItem.appendChild(statusCheckboxes);
-     taskList.appendChild(taskItem);
-  }
-  inputList.value = "";
-}
-=======*/
-import { daysStay } from "./daysStay.js";
-import { deleteTaskHandler } from "./deleteTaskHandler.js";
-
-const inputList = document.getElementById("task-storage");
-const inputContent = document.getElementById("task-content");
-const inputDate = document.getElementById("task-date");
-
-const taskSection = document.createElement("section");
-
-taskSection.innerHTML = `<ul class="task-list"></ul>`;
-export const taskList = taskSection.querySelector(".task-list");
-const body = document.querySelector("body");
-body.appendChild(taskSection);
-
-export const taskButton = document.getElementsByClassName("task-button")[0];
-export const deleteBtn = document.getElementsByClassName("delete-task");
-const inputDueDate = document.getElementById("task-due-date");
-
-export function addList() {
-  if (
-    inputList.value === "" ||
-    inputContent.value === "" ||
-    inputDate.value === ""
-  ) {
-    alert("Please, write something.");
-  } else {
-    const taskItem = document.createElement("li");
-    taskItem.classList.add("task-list__task");
-    taskItem.innerHTML = inputList.value;
-    taskItem.dataset.duedate = inputDate.value;
-
-    const taskContent = document.createElement("p");
-    taskContent.classList.add("task-list__content");
-    taskContent.innerHTML = inputContent.value;
-
-    const taskDate = document.createElement("p");
-    taskDate.classList.add("task-list__date");
-    taskDate.innerHTML = inputDate.value;
-
-    const remainingDays = daysStay(inputDate.value);
-    const taskDays = document.createElement("p");
-    taskDays.classList.add("task-list__date__due");
-    taskDays.innerHTML = `Days remaining: ${remainingDays}`;
 
     taskItem.appendChild(taskContent);
     taskItem.appendChild(taskDate);
